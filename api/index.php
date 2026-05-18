f76817ff71a70655a33b90e342b184ea2ed7c7ae
Pramadiant <apramadiantoputra@gmail.com>
2026-05-18 08:51:01 +0700

    Fix Vercel configuration using functions and runtime 0.7.3

--- api/index.php
+++ api/index.php
@@ -1,32 +1,3 @@
 <?php
-use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
-
-// Register the Composer autoloader
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// --- VERCEL SPECIFIC OVERRIDES ---
-// Vercel serverless functions only allow writing to /tmp
-$app->useStoragePath('/tmp/storage');
-
-// Create required directories inside /tmp
-$directories = [
-    'app',
-    'framework/views',
-    'framework/cache',
-    'framework/cache/data',
-    'framework/sessions',
-    'logs'
-];
-foreach ($directories as $dir) {
-    if (!is_dir('/tmp/storage/' . $dir)) {
-        mkdir('/tmp/storage/' . $dir, 0777, true);
-    }
-}
-// ---------------------------------
-
-$app->handleRequest(Request::capture());
+require __DIR__ . '/../public/index.php';
